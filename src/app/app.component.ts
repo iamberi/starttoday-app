a24b8a451ef50f0245131a7072213c4511e49f37
Kim Wehnes <kim.wehnes@t-online.de>
2021-04-28 21:57:06 +0200

    Service Worker Anpassung Anfang

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { SwUpdate } from '@angular/service-worker';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'starttoday-app';
+
+  update: boolean = false;
+
+  contructor(updates: SwUpdate){
+    updates.available.subscribe(event=>{
+      this.update = true;
+    })
+  }
 }

107f6191829bc3e6460f9a30e00f06dac07aeac8
JuliaFischinger <48588116+JuliaFischinger@users.noreply.github.com>
2021-04-30 12:19:22 +0200

    WT5-53 | Implement Firebase
    Datenbank mit Test eingerichtet

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAHKxyg0kuByvsXngM7RuBDmP3lmRgrkYM",
+    authDomain: "starttoday-app.firebaseapp.com",
+    projectId: "starttoday-app",
+    storageBucket: "starttoday-app.appspot.com",
+    messagingSenderId: "903772913531",
+    appId: "1:903772913531:web:9676cd5947131fd6f747de",
+    measurementId: "G-7ML1JDDCJD"
+  },
 };

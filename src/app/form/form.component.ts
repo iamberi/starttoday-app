8fe320106f6e5c4af8c3aa6bc3e5de8333cb7f05
NWiesler <48588140+NWiesler@users.noreply.github.com>
2021-05-04 17:19:33 +0200

    WT5-5 | Registrierung Logik
    logik stimmt, submit button stimmt noch nicht

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -8,7 +8,7 @@ import { User } from '../user';
   styleUrls: ['./form.component.scss']
 })
 export class FormComponent implements OnInit {
-  model = new User (24, 'max', 'mustermann', 'info@mail', 'password');
+  model = new User (24, '', '', '', '');
 
   submitted = false;
 
@@ -16,6 +16,10 @@ export class FormComponent implements OnInit {
 
   // TODO: Remove this when we're done
   get diagnostic() { return JSON.stringify(this.model); }
+
+  newUser(){
+    this.model = new User(42, '', '', '', '');
+  }
 
   skyDog(): User {
     const myUser =  new User(42, 'hanna', 'musterfrau', 'info@mail', 'password');

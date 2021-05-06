c0214fd1ccb4268f3090e96bfe510964aa2f110e
JuliaFischinger <48588116+JuliaFischinger@users.noreply.github.com>
2021-05-06 16:17:12 +0200

    WT5 | Merge Konflikte behoben

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,7 @@
 import { FirebaseService } from './services/firebase.service';
-import { TestModel } from './test-model';
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-root',
@@ -13,24 +11,12 @@ import { map } from 'rxjs/operators';
 export class AppComponent implements OnInit {
   title = 'starttoday-app';
   isSignedIn = false
-  
-  item: TestModel = {};
-  testModelCollection: AngularFirestoreCollection<any>;
-  list: Observable<TestModel[]>;
 
   constructor(
     private afs: AngularFirestore, public FirebaseService : FirebaseService) {
-      this.testModelCollection = afs.collection<TestModel>('TestModel');
     }
 
     ngOnInit(): void {
-      this.list = this.testModelCollection.snapshotChanges().pipe(
-        map(actions => actions.map(a => {
-          const data = a.payload.doc.data() as TestModel;
-          const id = a.payload.doc.id;
-          return { id, ...data };
-        }))
-      );
       if(localStorage.getItem('user')!== null)
       this.isSignedIn = true
       else
@@ -51,10 +37,5 @@ export class AppComponent implements OnInit {
 
     handleLogout(){
       this.isSignedIn = false
-    }
-
-    public save() {
-      this.testModelCollection.add(this.item);
-      this.item = {};
     }
 }

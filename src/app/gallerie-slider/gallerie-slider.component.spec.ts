f67d8ee0d9ee3868e53a40297a9e1d8c88057aad
iamberi <berivan.saricicek.1992@gmail.com>
2021-05-06 16:48:11 +0200

    gallerie-slider-5

--- src/app/gallerie-slider/gallerie-slider.component.spec.ts
+++ src/app/gallerie-slider/gallerie-slider.component.spec.ts
@@ -1,55 +1,44 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+const carouselSlide = document.querySelector('.carousel-slide');
+const carouselImages = document.querySelectorAll('.carousel-slide img');
 
-import { GallerieSliderComponent } from './gallerie-slider.component';
+//Buttons
+const prevBtn = document.querySelector('#prevBtn');
+const nextBtn = document.querySelector('#nextBtn');
 
-describe('GallerieSliderComponent', () => {
-  let component: GallerieSliderComponent;
-  let fixture: ComponentFixture<GallerieSliderComponent>;
+//Counter
+let counter = 1;
+const size = carouselImages[0].clientWidth;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ GallerieSliderComponent ]
-    })
-    .compileComponents();
-  });
+carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(GallerieSliderComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+//Buttons Listeners
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+nextBtn.addEventListener('click' ,()=> {
+  if (counter >= carouselImages.length - 1) return;
+  carouselSlide.style.transition = "transform 0.4s ease-in-out";
+  counter++;
+  carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
 });
 
+prevBtn.addEventListener('click' ,()=> {
+  if (counter <= 0) return;
+  carouselSlide.style.transition = "transform 0.4s ease-in-out";
+  counter++;
+  carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 
-var slideIndex = 1;
-showSlides(slideIndex);
+});
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+carouselSlide.addEventListener('transitioned' ,()=> {
+  if (carouselImages[counter].id === 'lastClone') {
+    carouselSlide.style.transform = "none";
+    counter = carouselImages.length -2;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+  if (carouselImages[counter].id === 'firstClone') {
+    carouselSlide.style.transform = "none";
+    counter = carouselImages.length counter;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
+
+});

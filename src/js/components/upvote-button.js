11c7f5d8a8224a59eb54fee1e1f49e3aa7ba3167
NWiesler <48588140+NWiesler@users.noreply.github.com>
2021-05-11 11:27:33 +0200

    WT5-29 / WT5-7 | Funktion Upvote Button

--- src/js/components/upvote-button.js
+++ src/js/components/upvote-button.js
@@ -1,7 +1,37 @@
+/*
+var upvoteButton = document.querySelector('.upvote-button-wrapper') // Using a class instead, see note below.
+upvoteButton.classList.toggle('upvote-button-wrapper-clicked');
+var count = 1;
+function setColor(btn, color) {
+    var property = document.getElementById(btn);
+    if (count == 0) {
+        property.style.backgroundColor = "#FFFFFF"
+        count = 1;
+    }
+    else {
+        property.style.backgroundColor = "#7FFF00"
+        count = 0;
+    }
+}*/
+
 function upvoteUp(){
-  document.getElementsByClassName("upvote-button-wrapper").className = "upvote-button-wrapper upvote-button-wrapper-clicked";
+  div =  document.getElementById("upvote-button");
+  if (div.attr('class') == 'upvote-button-wrapper') {
+
+    div.removeClass('upvote-button-wrapper').addClass('upvote-button-wrapper-clicked');
+  } else {
+    div.removeClass('upvote-button-wrapper-clicked').addClass('upvote-button-wrapper');
+  }
 }
+
+
+/*
+function upvoteUp(){
+  document.getElementById("upvote-button").className = "upvote-button-wrapper upvote-button-wrapper-clicked";
+  document.getElementById("upvote-button").onclick = upvoteDown();
+}
+
 
 function upvoteDown(){
-
-}
+  document.getElementById("upvote-button").className = "upvote-button-wrapper upvote-button-wrapper-clicked";
+}*/

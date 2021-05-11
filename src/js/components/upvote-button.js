1ea5d2893812d08ece1638785d0f36832b5a1f17
NWiesler <48588140+NWiesler@users.noreply.github.com>
2021-05-11 16:33:38 +0200

    WT5-7 / WT5-29 | Upvote Test

--- src/js/components/upvote-button.js
+++ src/js/components/upvote-button.js
@@ -1,5 +1,16 @@
 function upvoteUp(){
-  var element = document.getElementById("upvote-button");
-  element.classList.toggle("upvote-button-wrapper-clicked");
+  if (this.className='upvote-button-wrapper'){
+    this.className+='upvote-button-wrapper upvote-button-wrapper-clicked';
+  }
+  else {
+    this.className+='upvote-button-wrapper';
+  }
 
 }
+/*function upvoteUp(){
+  var element = document.getElementsByTagName("button");
+  if (this.class="upvote-button-wrapper"){
+    console.log(this);
+    this.classList.toggle("upvote-button-wrapper-clicked");
+  }
+}*/

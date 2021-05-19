a0452197bd572e792a001d445774dc94229ebe5d
JuliaFischinger <48588116+JuliaFischinger@users.noreply.github.com>
2021-05-19 13:15:54 +0200

    WT5-8 | Challenges-Detail Test

--- src/app/challenge-detail/challenge-detail.component.ts
+++ src/app/challenge-detail/challenge-detail.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+/* import { Component, OnInit, Input } from '@angular/core';
+import { ChallengeService } from '../services/challenge.service';
+import { Router, ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'app-challenges-detail',
@@ -7,9 +9,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChallengesDetailComponent implements OnInit {
 
-  constructor() { }
+  challengeRef: any
+
+  constructor(
+    public challengeService: ChallengeService,
+    private act: ActivatedRoute,
+    private router: Router
+  ) { }
+  @Input() titleChallenge: string;
+  @Input() descriptionChallenge: string;
+  @Input() categoryChallenge: string;
+  @Input() locationChallenge: string;
+  @Input() setterChallenge: string;
+  @Input() dateChallenge: string;
+  @Input() socialpointsChallenge: string;
 
   ngOnInit(): void {
+    const id = this.act.snapshot.paramMap.get('id');
+    this.challengeService.getChallengeDoc(id).subscribe(res => {
+      this.challengeRef = res;
+        this.editForm = this.formBuilder.group({
+        title: [this.challengeRef.name],
+        description: [this.challengeRef.email],
+        category: [this.challengeRef.contact]
+      })
+    })
   }
 
 }
+ */
+
+
+import { ChallengeService } from './../services/challenge.service';
+import { Component, OnInit } from '@angular/core';
+import { Challenge } from '../models/challenge.model';
+
+@Component({
+  selector: 'app-challenges-detail',
+  templateUrl: './challenge-detail.component.html',
+  styleUrls: ['./challenge-detail.component.scss']
+})
+export class ChallengesDetailComponent implements OnInit {
+  private detailChallenge: any
+
+
+  constructor(private challengeService: ChallengeService) { }
+
+  ngOnInit(): void {
+    this.challengeService.getChallenge().subscribe(res => {
+      this.detailChallenge = res.map( e => {
+        console.log(e.payload.doc.data() as object);
+        return {
+          id: e.payload.doc.id,
+          ...(e.payload.doc.data() as object)
+        } as Challenge;
+      });
+    });
+  }

cddd11368af951599c44ea5ae34721b4f72bc933
JuliaFischinger <48588116+JuliaFischinger@users.noreply.github.com>
2021-05-27 14:55:30 +0200

    WT5 | Update environment
    databaseURL hinzugefügt

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,14 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyAHKxyg0kuByvsXngM7RuBDmP3lmRgrkYM",
-    authDomain: "starttoday-app.firebaseapp.com",
-    projectId: "starttoday-app",
-    storageBucket: "starttoday-app.appspot.com",
-    messagingSenderId: "903772913531",
-    appId: "1:903772913531:web:9676cd5947131fd6f747de",
-    measurementId: "G-7ML1JDDCJD"
+    apiKey: 'AIzaSyAHKxyg0kuByvsXngM7RuBDmP3lmRgrkYM',
+    authDomain: 'starttoday-app.firebaseapp.com',
+    databaseURL: 'https://starttoday-app-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'starttoday-app',
+    storageBucket: 'starttoday-app.appspot.com',
+    messagingSenderId: '903772913531',
+    appId: '1:903772913531:web:9676cd5947131fd6f747de',
+    measurementId: 'G-7ML1JDDCJD'
   }
 };
 

4a8ba95423acd4a9bd086cde04f402ca57e8c8ba
JuliaFischinger <48588116+JuliaFischinger@users.noreply.github.com>
2021-05-27 14:41:16 +0200

    WT5-51 | Anpassung

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyAHKxyg0kuByvsXngM7RuBDmP3lmRgrkYM",
-    authDomain: "starttoday-app.firebaseapp.com",
-    databaseURL: "https://starttoday-app-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "starttoday-app",
-    storageBucket: "starttoday-app.appspot.com",
-    messagingSenderId: "903772913531",
-    appId: "1:903772913531:web:9676cd5947131fd6f747de",
-    measurementId: "G-7ML1JDDCJD"
+    apiKey: 'AIzaSyAHKxyg0kuByvsXngM7RuBDmP3lmRgrkYM',
+    authDomain: 'starttoday-app.firebaseapp.com',
+    databaseURL: 'https://starttoday-app-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'starttoday-app',
+    storageBucket: 'starttoday-app.appspot.com',
+    messagingSenderId: '903772913531',
+    appId: '1:903772913531:web:9676cd5947131fd6f747de',
+    measurementId: 'G-7ML1JDDCJD'
   },
 };

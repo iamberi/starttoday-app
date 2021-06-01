1b6efac6fa7a53a9cce41a196a6c45df48f516e4
JuliaFischinger <48588116+JuliaFischinger@users.noreply.github.com>
2021-06-01 15:28:01 +0200

    WT5-51 | Tutorial Integrated File Upload
    https://dev.to/angular/implement-file-upload-with-firebase-storage-in-our-angular-app-the-simple-way-1ofi

---
+++ src/app/const/storage.const.js
@@ -0,0 +1,1 @@
+export const MEDIA_STORAGE_PATH = `starttoday/media/`;
--- src/app/probleme/create-problem/create-problem.component.ts
+++ src/app/probleme/create-problem/create-problem.component.ts
@@ -1,21 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ProblemService } from './../../services/problem.service';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from "@angular/router";
+import { Observable, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
+import { StorageService } from 'src/app/services/storage/storage.service';
+import { MEDIA_STORAGE_PATH } from '../../const/storage.const.js';
+import { UtilService } from '../../services/util/util.service';
 
 @Component({
   selector: 'app-create-problem',
   templateUrl: './create-problem.component.html',
   styleUrls: ['./create-problem.component.scss']
 })
-export class CreateProblemComponent implements OnInit {
+export class CreateProblemComponent implements OnInit, OnDestroy {
   public problemForm: FormGroup;
+  //
+  destroy$: Subject<null> = new Subject();
+  fileToUpload: File;
+  problemImagePreview: string | ArrayBuffer;
+  pictureForm: FormGroup;
+  submitted = false;
+  // problem: firebase.Problem;
 
   constructor(
     public problemService: ProblemService,
     public formBuilder: FormBuilder,
-    public router: Router
-  ) {
+    public router: Router,
+    private storageService: StorageService,
+    private readonly utilService: UtilService,
+  ) {}
+
+  ngOnInit(): void {
     this.problemForm = this.formBuilder.group({
       title: [''],
       description: [''],
@@ -25,19 +42,69 @@ export class CreateProblemComponent implements OnInit {
       votes: 0,
       statusvotes: 'vote',
       socialpoints: 3,
-      image: this.formBuilder.group({
-        downloadURL: [''],
-        path: [''],
-      }),
+      photo: [null, Validators.required],
+    });
+    this.problemForm
+    .get('photo')
+    .valueChanges.pipe(takeUntil(this.destroy$))
+    .subscribe((newValue) => {
+      this.handleFileChange(newValue.files);
     });
   }
 
-  ngOnInit(): void {
+  handleFileChange([problemImage]) {
+    this.fileToUpload = problemImage;
+    const reader = new FileReader();
+    reader.onload = (loadEvent) => (this.problemImagePreview = loadEvent.target.result);
+    reader.readAsDataURL(problemImage);
   }
+
+  postProblem() {
+    this.submitted = true;
+    const mediaFolderPath = `${MEDIA_STORAGE_PATH}/images/${new Date().getTime()}`; //`images/${new Date().getTime()}_${file.name}`
+
+    const { downloadUrl$ } = this.storageService.uploadFileAndGetMetadata(
+      mediaFolderPath,
+      this.fileToUpload,
+    );
+
+//    this.uploadProgress$ = uploadProgress$;
+
+    downloadUrl$
+      .pipe(
+        takeUntil(this.destroy$)/* ,
+        catchError((error) => {
+          this.snackBar.open(`${error.message} 😢`, 'Close', {
+            duration: 4000,
+          });
+          return;
+        }), */
+      )
+      .subscribe((downloadUrl) => {
+        this.submitted = false;
+        this.router.navigate([`/probleme`]);
+      });
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next(null);
+  }
+
+  private image(photoControl: AbstractControl): { [key: string]: boolean } | null {
+    if (photoControl.value) {
+      const [problemImage] = photoControl.value.files;
+      return this.utilService.validateFile(problemImage)
+        ? null
+        : {
+            image: true,
+          };
+    }
+    return;
+  }
+
 
   onSubmit() {
     this.problemService.createProblem(this.problemForm.value);
     this.router.navigate(['/probleme']);
-   };
-
+   }
 }

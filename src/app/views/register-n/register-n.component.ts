04c257b42b20d9871382eee9f87faf1d5fb6b087
Kim Wehnes <kim.wehnes@t-online.de>
2021-06-07 09:27:02 +0200

    anpassungen

--- src/app/views/register-n/register-n.component.ts
+++ src/app/views/register-n/register-n.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { User } from '../../services/user';
+//import { User } from '../../services/user';
 
 
 @Component({
@@ -8,27 +8,16 @@ import { User } from '../../services/user';
   styleUrls: ['./register-n.component.scss']
 })
 export class RegisterNComponent implements OnInit {
-  model = new User (24, '', '', '', '','');
-  submitted = false;
 
-  // TODO: Remove this when we're done
-  //get diagnostic() { return JSON.stringify(this.model); }
 
-  newUser():void {
-    this.model = new User(42, '', '', '','', '');
-  }
 
 
   onSubmit():void {
-    this.submitted = true;
+
     return;
   }
 
-  skyDog(): User {
-    const myUser =  new User(42, 'hanna', 'musterfrau', 'info@mail', 'info@mail','password');
-    console.log('My hero is called ' + myUser.vorname); // "My hero is called SkyDog"
-    return myUser;
-  }
+
 
   constructor() { }
 

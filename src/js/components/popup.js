0407e79b931e333329cc222bba7b1f70876bdd10
NWiesler <48588140+NWiesler@users.noreply.github.com>
2021-06-10 15:01:17 +0200

    WT5-126 | Beispielhafte Implementierung

--- src/js/components/popup.js
+++ src/js/components/popup.js
@@ -2,13 +2,13 @@
 function teilnehmen(){
     var status = document.getElementById("teilnehmen-popup")
     status.className = "popover-wrapper-visible";
-  
+
   }
-  
+
   function closeTeilnehmen(){
     var status = document.getElementById("teilnehmen-popup")
     status.className = "popover-wrapper";
-  
+
   }
 
   function sendMail(){
@@ -16,7 +16,7 @@ function teilnehmen(){
     var mailto="mailto:" +mail;
 
     var button = document.getElementById("sendTeilnehmenMail");
-    button.href=mailto; 
+    button.href=mailto;
   }
 
 
@@ -25,21 +25,31 @@ function teilnehmen(){
     var status = document.getElementById("verfizierung-popup")
     status.className = "popover-wrapper-visible";
   }
-  
+
   function CloseVerifizieren(){
     var status = document.getElementById("verfizierung-popup")
     status.className = "popover-wrapper";
   }
-  
+
 
 // Lösung einreichen
   function Solution() {
     var status = document.getElementById("solution-popup")
     status.className = "popover-wrapper-visible";
   }
-  
+
   function closeSolution(){
     var status = document.getElementById("solution-popup")
     status.className = "popover-wrapper";
   }
-  
+
+// Erklärung Social Points
+function openExplainSocialPoints() {
+  var status = document.getElementById("socialpoints-popup")
+  status.className = "popover-wrapper-visible";
+}
+
+function closeExplainSocialPoints(){
+  var status = document.getElementById("socialpoints-popup")
+  status.className = "popover-wrapper";
+}
